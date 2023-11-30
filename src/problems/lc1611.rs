f0509583d2c406c47a7e22bcf0277ec67b93c470
Zhifeng Wang <51321751+zf-w@users.noreply.github.com>
2023-11-30 08:41:04 -0600

    Add lc0191, 1611;

---
+++ src/problems/lc1611.rs
@@ -0,0 +1,15 @@
+//! ## Leetcode 1611. Minimum One Bit Operations to Make Integers Zero
+//! - `Hard`; `Learned`; `2023-11-30`;
+//! 
+//! I didn't fully understand the mechanism of the recursion formula. I improved the leftmost bit-checking part a bit. I guess, to solve problems like this, trying to find the patterns of numbers is indeed important.
+
+pub fn minimum_one_bit_operations(n: i32) -> i32 {
+    if n <= 1 {
+        return n;
+    }
+    let mut cp = n;
+    while cp & (cp - 1) > 0 {
+        cp &= cp - 1;
+    }
+    return (2 * cp - 1) - self::minimum_one_bit_operations(n - cp);
+}
