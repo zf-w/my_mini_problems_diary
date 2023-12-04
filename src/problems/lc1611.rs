d8e0882017ec5a9dc8f6393cd80b521f24625703
Zhifeng Wang <51321751+zf-w@users.noreply.github.com>
2023-12-04 00:21:50 -0600

    Fixing mismatched dates due to late logging

--- src/problems/lc1611.rs
+++ src/problems/lc1611.rs
@@ -1,5 +1,5 @@
 //! ## Leetcode 1611. Minimum One Bit Operations to Make Integers Zero
-//! - `Hard`; `Learned`; `2023-11-30`;
+//! - `Hard`; `Learned`; `2023-11-29`;
 //! 
 //! I didn't fully understand the mechanism of the recursion formula. I improved the leftmost bit-checking part a bit. I guess, to solve problems like this, trying to find the patterns of numbers is indeed important.
 
