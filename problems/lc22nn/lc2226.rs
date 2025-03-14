1c847921f44c5de660d66decc31d7ad4a1237980
Zhifeng Wang <51321751+zf-w@users.noreply.github.com>
2025-03-14 17:54:02 -0500

    [y2025m03d14] add lc2226.

--- problems/lc22nn/lc2226.rs
+++ problems/lc22nn/lc2226.rs
@@ -1,10 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+//! # Leetcode 2226. Maximum Candies Allocated to K Children
+//! https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+//! - `Medium`; `y2025m03d14`; `Independently Solved`; `7ms`; `3.5mb`; `2 attempt`;
+//! Topics: binary_search.
+
 pub fn maximum_candies(candies: Vec<i32>, k: i64) -> i32 {
-    let candy_sum: i32 = candies.iter().sum();
+    let candy_sum: i64 = candies.iter().map(|v| -> i64 { (*v) as i64 }).sum(); // Forgot to take sum in `i64`` instead of `i32``.
     let mut begin_num: i64 = 0;
-    let mut end_num: i64 = ((candy_sum as i64) / k) + 1;
+    let mut end_num: i64 = ((candy_sum as i64) / k) + 1; // Leading to a wrong end boundary.
 
     let check_closure = |mid_num: i64| -> bool {
         let mut child_satisfied_num: i64 = 0;
@@ -17,6 +19,7 @@ pub fn maximum_candies(candies: Vec<i32>, k: i64) -> i32 {
         false
     };
 
+    // println!("{} {}", check_closure(1000000), end_num);
     while begin_num < end_num {
         let mid_num = (begin_num + end_num) / 2;
         if mid_num == 0 || check_closure(mid_num) == true {
